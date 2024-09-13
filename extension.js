3571247b79a465f1bbe3f574c4ff6e4fb2fcb636
Al-Amin Islam Hridoy <152881081+Hridoy-EL@users.noreply.github.com>
2024-09-13 16:03:06 +0600

    new themes added

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 const vscode = require('vscode');
-function a(c) {
-    let m = vscode.commands.registerCommand('buzzokai.mono', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Mono Dimmed', !0)),
-        r = vscode.commands.registerCommand('buzzokai.reversed', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Reversed Dimmed', !0)),
-        g = vscode.commands.registerCommand('buzzokai.github', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai GitHub Dimmed', !0)),
-        d = vscode.commands.registerCommand('buzzokai.material', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Material Dimmed', !0));
-    c.subscriptions.push(m, r, g, d);
+function activate(context) {
+    let mono = vscode.commands.registerCommand('buzzokai.mono', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Mono Dimmed', true)),
+        reversed = vscode.commands.registerCommand('buzzokai.reversed', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Reversed Dimmed', true)),
+        github = vscode.commands.registerCommand('buzzokai.github', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai GitHub Dimmed', true)),
+        material = vscode.commands.registerCommand('buzzokai.material', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Material Dimmed', true)),
+        materialBlue = vscode.commands.registerCommand('buzzokai.materialBlue', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Material Blue', true)),
+        shopifyBlue = vscode.commands.registerCommand('buzzokai.shopifyBlue', () => vscode.workspace.getConfiguration().update('workbench.colorTheme', 'Buzzokai Shopify Blue', true));
+    context.subscriptions.push(mono, reversed, github, material, materialBlue, shopifyBlue);
 }
-function d() {}
-module.exports = {a,d};
+function deactivate() {}
+module.exports = {activate,deactivate};
